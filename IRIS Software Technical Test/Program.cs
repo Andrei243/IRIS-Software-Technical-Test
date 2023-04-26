b50aa8955128fc28daef40bb923a198ee184e967
andre <andre@MANDEL-PC>
2023-04-26 20:05:41 +0300

    Add project files.

---
+++ IRIS Software Technical Test/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IRIS_Software_Technical_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
